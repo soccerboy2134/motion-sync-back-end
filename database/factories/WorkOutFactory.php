e0e5533cb94c2048531af6f9db897caa8ee23aed
Rohan Mahadew <65652122+RohanMahadew@users.noreply.github.com>
2026-06-04 14:26:04 +0200

    Different calculation for points

--- database/factories/WorkOutFactory.php
+++ database/factories/WorkOutFactory.php
@@ -16,17 +16,49 @@ class WorkOutFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    // public function definition(): array
+    // {
+    //     // Yes, UserIds can be grabbed in a much better way BUT it is just not optimized.
+    //     // I don't want to murder my PC (they are innocent!) and this works. 
+    //     // We know theres 100 seeded users anyway...
+
+    //     $length = fake()->numberBetween(1000, 10000);
+    //     $speed = fake()->numberBetween(1, 7) . '.' . fake()->numberBetween(0, 9);
+    //     $type = fake()->randomElement(['walking', 'running']);
+    //     $points = $length * $speed;
+    //     $points = ($type == 'running') ? $points * 2 : $points;
+
+    //     return [
+    //         'user_id' => fake()->numberBetween(1, 100),
+    //         'length' => $length,
+    //         'speed' => $speed,
+    //         'type' => $type,
+    //         'points' => $points,
+    //     ];
+    // }
     public function definition(): array
     {
         // Yes, UserIds can be grabbed in a much better way BUT it is just not optimized.
         // I don't want to murder my PC (they are innocent!) and this works. 
         // We know theres 100 seeded users anyway...
-
+        
         $length = fake()->numberBetween(1000, 10000);
-        $speed = fake()->numberBetween(1, 7) . '.' . fake()->numberBetween(0, 9);
-        $type = fake()->randomElement(['walking', 'running']);
-        $points = $length * $speed;
-        $points = ($type == 'running') ? $points * 2 : $points;
+
+        $speed = fake()->randomFloat(1, 2, 15);
+
+        $type = match (true) {
+            $speed < 4.5 => 'walking',
+            $speed < 10.0 => 'running',
+            default => 'sprinting',
+        };
+
+        $multiplier = match ($type) {
+            'walking' => 1,
+            'running' => 2,
+            'sprinting' => 3,
+        };
+
+        $points = round(($length * $speed * $multiplier) / 100);
 
         return [
             'user_id' => fake()->numberBetween(1, 100),
